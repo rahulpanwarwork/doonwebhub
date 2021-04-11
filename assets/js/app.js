44e48d32bb8f1aa0dccf3f54fd5b25325aa1d705
Rahul Panwar <rahulpanwarwork@gmail.com>
2021-04-11 22:24:44 +0530

    Home Version 1

---
+++ assets/js/app.js
@@ -0,0 +1,45 @@
+// OffCanvas Navigation Menu on Mobile Screen Start
+function darken_screen(yesno) {
+  if (yesno == true) {
+    document.querySelector(".screen-darken").classList.add("active");
+  } else if (yesno == false) {
+    document.querySelector(".screen-darken").classList.remove("active");
+  }
+}
+
+function close_offcanvas() {
+  darken_screen(false);
+  document.querySelector(".mobile-offcanvas.show").classList.remove("show");
+  document.body.classList.remove("offcanvas-active");
+  document.querySelector(".mobile-toggle").classList.add("bg-white");
+}
+
+function show_offcanvas(offcanvas_id) {
+  darken_screen(true);
+  document.getElementById(offcanvas_id).classList.add("show");
+  document.body.classList.add("offcanvas-active");
+  document.querySelector(".mobile-toggle").classList.remove("bg-white");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("[data-trigger]").forEach(function (everyelement) {
+    let offcanvas_id = everyelement.getAttribute("data-trigger");
+    everyelement.addEventListener("click", function (e) {
+      e.preventDefault();
+      show_offcanvas(offcanvas_id);
+    });
+  });
+
+  document.querySelectorAll(".btn-close").forEach(function (everybutton) {
+    everybutton.addEventListener("click", function (e) {
+      close_offcanvas();
+    });
+  });
+
+  document
+    .querySelector(".screen-darken")
+    .addEventListener("click", function (event) {
+      close_offcanvas();
+    });
+});
+// OffCanvas Navigation Menu on Mobile Screen End
