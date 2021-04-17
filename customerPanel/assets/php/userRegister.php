4f27b0f5e0eaedda8f6d8789820bc17fb8ca49e8
Rahul Panwar <rahulpanwarwork@gmail.com>
2021-04-17 20:14:46 +0530

    comment removed

--- customerPanel/assets/php/userRegister.php
+++ customerPanel/assets/php/userRegister.php
@@ -11,7 +11,9 @@ $sqlCheck = "SELECT * FROM userdetails WHERE email = '$email'";
 $sql = "INSERT INTO userdetails (username, email, passkey) VALUES('$username', '$email', '$password')";
 
 if ($conn->query($sql) === TRUE) {
-    // echo "New record created successfully";
+    echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
+?>
